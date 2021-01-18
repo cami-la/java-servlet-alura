1b32eefd9c9406598481eb084ab5587ccdee0a36
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-01-18 16:07:20 -0300

    util class 01

--- gerenciador/src/site/camila/gerenciador/servlet/OiMundoServlet.java
+++ gerenciador/src/site/camila/gerenciador/servlet/OiMundoServlet.java
@@ -1,20 +1,29 @@
 package site.camila.gerenciador.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet(urlPatterns = )
+@WebServlet(urlPatterns = "/oi")
 public class OiMundoServlet extends HttpServlet {
 	
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) {
-			
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Oi mundo, parabéns você escreveu seu primeiro servlet!");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("O servlet \"OiMundoServlet\" foi chamado. ");
+
 	}
 
 }
