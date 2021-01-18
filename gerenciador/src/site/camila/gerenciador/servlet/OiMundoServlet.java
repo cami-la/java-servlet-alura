d07a977a23abab0e36f3bf2d164a22ba6f734474
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-01-18 12:03:36 -0300

    first commit

---
+++ gerenciador/src/site/camila/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,20 @@
+package site.camila.gerenciador.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns = )
+public class OiMundoServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) {
+			
+	}
+
+}
