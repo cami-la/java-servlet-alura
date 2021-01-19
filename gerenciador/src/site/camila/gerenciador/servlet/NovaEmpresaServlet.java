dd7f5d368e7360181e4a5017d7aa5a55db30e617
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-01-19 18:01:10 -0300

    util class 02

---
+++ gerenciador/src/site/camila/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,28 @@
+package site.camila.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NovaEmpresaServlet
+ */
+@WebServlet("/NovaEmpresaServlet")
+public class NovaEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Cadastrando nova empresa");
+		
+		String nomeEmpresa = request.getParameter("nome");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com sucesso!</html></body>");
+	}
+
+}
