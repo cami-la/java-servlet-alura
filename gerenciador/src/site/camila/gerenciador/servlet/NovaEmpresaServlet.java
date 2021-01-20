0657d97ffdc09c757461051e5cb61d37a963854b
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-01-20 15:39:57 -0300

    util class 02.07 - Apenas POST

--- gerenciador/src/site/camila/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/site/camila/gerenciador/servlet/NovaEmpresaServlet.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa");
 		
 		String nomeEmpresa = request.getParameter("nome");
