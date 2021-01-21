0106179cf5f798ab544a8cc38f719014bdd6d9fb
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-01-21 11:46:15 -0300

    util class 03.05 - Listando empresas com servlets

--- gerenciador/src/site/camila/gerenciador/servlet/Banco.java
+++ gerenciador/src/site/camila/gerenciador/servlet/Banco.java
@@ -7,6 +7,17 @@ public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<Empresa>();
 	
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("Alura");
+		
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Caelum");	
+		
+		lista.add(empresa);
+		lista.add(empresa2);
+	}
+	
 	public void adiciona(Empresa empresa) {
 		lista.add(empresa);
 	}
