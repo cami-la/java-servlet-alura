83daf93fac5aedb2fd476dbf87544bbba4337927
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-01-21 17:29:50 -0300

    util class 04.08 - JSP para listar empresas

--- gerenciador/src/site/camila/gerenciador/servlet/ListaEmpresaServlet.java
+++ gerenciador/src/site/camila/gerenciador/servlet/ListaEmpresaServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,16 +24,11 @@ public class ListaEmpresaServlet extends HttpServlet {
 
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
-
-		PrintWriter out = response.getWriter();
-
-		out.println("<html><body>");
-		out.println("<ul>");
-		for (Empresa empresa : lista) {
-			out.println("<li>" + empresa.getNome() + "</li>");
-		}
-		out.println("</ul>");
-		out.println("</body></html>");
+		
+		request.setAttribute("empresas", lista);
+		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+		rd.forward(request, response);
+		
 	}
 
 }
